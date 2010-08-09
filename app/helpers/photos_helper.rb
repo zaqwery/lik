9ed670ea84034a612a7faad342ff6f2b91719f90
zaqwery <zaqwery@NEC.(none)>
2010-08-09 20:25:05 +0300

    new layout

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,7 +1,14 @@
 # Helper methods defined here can be accessed in any controller or view in the application
 
 Lik.helpers do
-  # def simple_helper_method
-  #  ...
-  # end
+
+  def project_image
+    @project = Project.find(params[:id])
+    @photos = @project.photos
+    @photos.each do |img|
+      image = MiniMagick::Image.from_file(img.image)
+      image[:height]
+    end
+  end
+
 end
