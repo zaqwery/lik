c19aae15349de00e94d0bdf3b1a98aca63f0ea35
zaqwery <zaqwery@NEC.(none)>
2010-08-11 19:16:54 +0300

    transitions, project_index, partners

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,7 +1,14 @@
 # Helper methods defined here can be accessed in any controller or view in the application
 
 Lik.helpers do
-  # def simple_helper_method
-  #  ...
-  # end
+
+  def project_thumb
+    @project_images = []
+    projects = Project.find(:all)
+    projects.each do |project|
+      @project_images << project.photos.first.image.thumb
+    end
+  end
+
+
 end
