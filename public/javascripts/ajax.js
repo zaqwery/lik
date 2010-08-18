444355d711d0e0226a373fb04248961509b72cd7
zaqwery <zaqwery@NEC.(none)>
2010-08-18 19:33:43 +0300

    styles and jquery

--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
 
-  $("#main a").click(function(){	
+  var cycleWin = $(function() {
+          $('#images').cycle();
+      });
+
+  $(cycleWin).bind();
+  $(cycleWin).unbind();
+
+  $(".thumb").click(function(){	
     var largePath = $(this).attr("href");
+    
     $(".content").attr({ src: largePath });
     return false;
   });
