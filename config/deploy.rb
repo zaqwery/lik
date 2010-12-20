a90c1ad3b1ff50c716da21d225f555d67313e31c
Illia <zaqwery@gmail.com>
2010-12-20 22:10:00 +0200

    production v 1.0.1

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,27 +1,46 @@
+require "bundler/capistrano"
+
 set :application, "LIK"
 set :repository,  "git@github.com:zaqwery/lik.git"
 
-set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-set :deploy_to, "~/projects/lik-odessa"
-
-role :web, "lik.od.ua"                          # Your HTTP server, Apache/etc
-role :app, "lik.od.ua"                          # This may be the same as your `Web` server
-role :db,  "lik.od.ua", :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
+dpath = "/home/hosting_lik-odessa/projects/lik-odessa"
 
 set :user, "hosting_lik-odessa"
-set :scm_username, "zaqwery" 
 set :use_sudo, false
+set :deploy_to, dpath
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :scm, :git
 
+role :web, "lithium.locum.ru"                          # Your HTTP server, Apache/etc
+role :app, "lithium.locum.ru"                          # This may be the same as your `Web` server
+role :db,  "lithium.locum.ru", :primary => true # This is where Rails migrations will run
+
+#after "deploy:update_code", :copy_database_config
+
+#task :copy_database_config, roles => :app do
+ # db_config = "#{shared_path}/database.yml"
+  #run "cp #{db_config} #{release_path}/config/database.yml"
+#end
+
+#set :unicorn_rails, "/var/lib/gems/1.8/bin/unicorn_rails"
+#set :unicorn_conf, "/etc/unicorn/likp-odessa.hosting_lik-odessa.rb"
+#set :unicorn_pid, "/var/run/unicorn/lik-odessa.hosting_lik-odessa.pid"
+
+# - for unicorn - #
 namespace :deploy do
-   task :start do ; end
-   task :stop do ; end
-   task :restart, :roles => :app, :except => { :no_release => true } do
-     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-   end
+  desc "Start application"
+  task :start, :roles => :app do
+     run "cd #{deploy_to}/current && padrino start -e production"
+  end
+
+  desc "Stop application"
+  task :stop, :roles => :app do
+    run "cd #{deploy_to}/current && padrino stop -e production"
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    deploy.stop
+    deploy.start
+  end
 end
